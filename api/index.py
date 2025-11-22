5b494a0c0ef5531549ec64d797068817daf01d61
Vihaan Kanwar <vihaankanwar35@gmail.com>
2025-11-22 10:35:27 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-client = genai.Client(api_key="AIzaSyAbWBbH02kg4uRUygKbC2rsub7I-7izFg8")
+client = genai.Client(api_key="AIzaSyCQ25UsuYUAyyKcUAaMwKmutUVewTA54QQ")
 model_id = "gemini-2.5-flash-preview-09-2025" 
 
 def get_combined_intel(query):
